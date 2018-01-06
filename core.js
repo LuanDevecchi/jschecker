d23e081201e8bccac7d095cebe54637d76ace584
luan devecchi <sillonfox@live.com>
2018-01-05 23:03:12 -0200

    Update core.js

--- core.js
+++ core.js
@@ -1,8 +1,7 @@
 var request = require('request');
 var main = require('./main.js');
 var fs = require('fs');
-console.log("\x1b[36m" + 'Pedidos Já Checker' , '\x1b[0m');
-console.log('Programado por Luan Devecchi\n \n ');
+console.log("\x1b[36m" + 'Node JS Checker' , '\x1b[0m');
 
 
 main.coreStart();
